1ff899179329e2fcdbc0701a9a59651c04345956
ssb000ss <ssb000ss@gmail.com>
2019-02-22 05:46:30 -0600

    Triangle loves was implemented

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
+
+
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+    let result = preferences.reduce(((previousValue, currentValue, currentIndex, array) => {
+        if (array[array[currentValue - 1] - 1] === currentIndex + 1) previousValue++;
+        return previousValue;
+    }), 0) / 3;
+    return Math.floor(result);
 };
+
